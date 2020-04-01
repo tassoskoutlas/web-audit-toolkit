84d746e1316d321d9c5717a18d5228cade4ed1f8
Tassos Koutlas <tassos.koutlas@ffwagency.com>
2020-04-01 17:01:24 +0000

    Rename urlgrab to contentauditor and remove link dumping functionality

--- contentauditor.sh
+++ contentauditor.sh
@@ -12,7 +12,7 @@ WAIT="0.01"
 FILE=$(echo "$1" | cut -d '/' -f3)
 
 # Exclude donwloading these resources.
-EXCLUDE="js,css,jpg,jpeg,gif,png,ico,svg,mp4,mp3"
+EXCLUDE="js,css,jpg,jpeg,gif,png,ico,svg,mp4,mp3,rss"
 
 # If wget wait set at command line use it.
 if [[ ! -z $2 ]]; then
@@ -26,18 +26,4 @@ wget -r -nd --spider --force-html -R "${EXCLUDE}" "${URL}" -w "${WAIT}" 2>&1 | e
 
 echo "Done"
 
-# echo "Starting link audit ..."
 
-# # Loop through the internal urls
-# while read url; do
-# 	echo "${url}"
-# done < "${FILE}-internal-urls.txt"
-
-# # Clean up the file
-# sed -i "/Visible/d" "${FILE}-temp.txt"
-# sed -i "/Hidden/d" "${FILE}-temp.txt"
-# sed -i '/^$/d' "${FILE}-temp.txt"
-
-# cat "${FILE}-temp.txt" | sort | uniq > "${FILE}-all-urls.txt"
-
-# echo "Done"
